e05f0636d207ae338377b1873911429c30f1b8bc
Allan Assis <allan-bdc@hotmail.com>
2022-07-15 00:52:16 -0300

    Catchau

---
+++ main/__init__.py
@@ -0,0 +1,10 @@
+import jinja2
+
+env = jinja2.Environment(
+    loader=jinja2.PackageLoader("main", "static"),
+    autoescape=jinja2.select_autoescape()
+)
+
+template = env.get_template("static/index.jinja")
+
+print(template.render(footer="footer text", body="body text", header="header text"))
